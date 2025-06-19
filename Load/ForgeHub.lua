7e8649b439b405c73cffa12d56a89bfa78779bfd
ZeoHub <romanozchristian0@gmail.com>
2025-06-19 22:00:48 +0800

    Update ForgeHub.lua

--- Load/ForgeHub.lua
+++ Load/ForgeHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGeto("https://raw.githubusercontent.com/ZeoHub/GrowAGarden/refs/heads/main/ForgeHub.lua"))()
+loadstring(game:HttpGeto("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Key/ForgeHub.lua"))()
