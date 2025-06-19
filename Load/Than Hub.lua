0f69356be8ebc26b33e2f11462e7058e9562f975
ZeoHub <romanozchristian0@gmail.com>
2025-06-19 21:59:20 +0800

    Update Than Hub.lua

--- Load/Than Hub.lua
+++ Load/Than Hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGarden/refs/heads/main/ThanHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/NoKey/ThanHub.lua"))()
