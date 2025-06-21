fbbe43ebf52cc3e92c236fe7468221fb3d1e003e
ZeoHub <romanozchristian0@gmail.com>
2025-06-21 15:35:51 +0800

    Update Pet Spawner.lua

--- Load/Pet Spawner.lua
+++ Load/Pet Spawner.lua
@@ -1,2 +1,2 @@
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Visual/Pet%20Spawner.lua"))() 
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Visual/Pet%20Spawner.lua", true))() 
 Spawner.Load()
