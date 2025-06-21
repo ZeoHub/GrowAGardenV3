686ed495d3ccefea881360fe8706373d87aff0cc
ZeoHub <romanozchristian0@gmail.com>
2025-06-22 00:08:19 +0800

    Update Pet Spawner.lua

--- Load/Pet Spawner.lua
+++ Load/Pet Spawner.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/imalwaysbeamediocre/PetSpawner/refs/heads/main/DarkScripts.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Visual/PetPlacer.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Visual/Pet%20Spawner.lua"))()
